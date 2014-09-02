e6a1def07981b4e30e8953061cb6207a13c240a0
bcarancibia <bcarancibia@gmail.com>
2014-09-01 21:26:53 -0400

    First HW

---
+++ Week_1_Assignment.R
@@ -0,0 +1,19 @@
+#####Versions of R and Rstudio####
+R version 3.1.1 (2014-07-10) -- "Sock it to Me"
+Copyright (C) 2014 The R Foundation for Statistical Computing
+Platform: x86_64-apple-darwin13.1.0 (64-bit)
+
+RStudio v0.98.1028
+
+####PSQL####
+Last login: Mon Sep  1 21:02:47 on ttys000
+'/Applications/Postgres.app/Contents/Versions/9.3/bin'/psql -p5432
+Bens-MacBook-Pro:~ bcarancibia$ '/Applications/Postgres.app/Contents/Versions/9.3/bin'/psql -p5432
+psql (9.3.5)
+Type "help" for help.
+
+####DMwR####
+> install.packages("DMwR")
+also installing the dependencies ‘gtools’, ‘gdata’, ‘Defaults’, ‘TTR’, ‘gplots’, ‘xts’, ‘quantmod’, ‘zoo’, ‘abind’, ‘ROCR’
+
+Number of observations in the data set is 401,146
