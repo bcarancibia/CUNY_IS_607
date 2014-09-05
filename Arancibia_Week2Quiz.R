75b79c488221bb48746bcc5f0914f837cff7f08f
bcarancibia <bcarancibia@gmail.com>
2014-09-05 17:09:43 -0400

    quiz week 2

---
+++ Arancibia_Week2Quiz.R
@@ -0,0 +1,103 @@
+#####Benjamin Arancibia######
+####Week 2 Quiz####
+
+####Question 1####
+
+v <- c(2, 4, 6, 7, 8, 8, 1, -5, 20, 9, 9, 9, 10, 1, 13, 14, 15, -4, -3, -1)
+
+####Question 2####
+
+c <- as.character(v)
+
+####Question 3####
+
+f <- factor(v)
+
+####Question 4####
+
+levels(f)
+
+####Question 5####
+x <- 3*(v^2)-(4*v) + 1
+
+####Question 6####
+B <- ((((t(X)*X))%^%-1)*((t(X))*y))
+
+####Question 7####
+nl = list(eliza=c(2, 3, 5, 6), ben=c(1, 4, 7, 8, 9), carlos=c("aa", "bb"))
+
+####Question 8####
+date <- seq(as.Date("2014/5/1"), as.Date("2014/5/20"), by="1 day")
+df <- data.frame(c, f, v, date)
+
+####Question 9####
+insertdf = dataframe(c = 1, f = 3, v = 13, date = as.Date("2014/5/21"))
+newdf <- rbind(df[1:4,], insertdg, data[4,] )
+
+####Question 10####
+getwd()
+csvfile = read.csv("temperatures.csv")
+
+####Question 11####
+setwd("<new path other directory>")
+tsvfile = read.table("measurements.txt")
+
+####Question 12####
+pipe_date <- read.table("http://science.nature.nps.gov/im/monitor/stats/R/data/MyData.txt", header=TRUE, sep="|")
+
+####Question 13####
+factorial <- function(x){
+  for(i in 1:x)
+    y <-y*((1:x[i]))
+}
+print(y)
+
+####Question 14####
+P = 1500
+r = .0324
+n = 12
+t = 6
+Final_Balance <- P(1 + r/n)nt
+
+####Question 15####
+
+y <- 1:20
+sum(y[-1][c9(False, False, True)])
+
+####Question 16####
+sigma <- function(x){
+  for (i in 1:x)
+    y <- 2%^%(1:x[i])
+}
+print(y)
+
+####Question 17####
+
+sigma <- function(x){
+  for (i in 1:x)
+    while i <= 11
+    y <- 2%^%(1:x[i])
+}
+print(y)
+####Question 18####
+x=2
+
+sum(x%^%(1:10))
+
+####Question 19####
+x0 = 20
+seq(x0, 50, 5)
+
+####Question 20####
+
+e <- c(1:10)
+d <- as.character(e)
+length(d)
+rep(d, 10)
+
+####Question 21####
+#this finds the slope if you want to find the length of a triangle just solve a^2+b^2=c^2
+a=
+b=
+c=
+(-b + c(-1, 1) * sqrt(b^2 - 4 * a * c))/(2 * a)
