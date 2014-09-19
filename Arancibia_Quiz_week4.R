982534961c6bfebff83c176a8cc316f914843e9c
bcarancibia <bcarancibia@gmail.com>
2014-09-19 17:30:33 -0400

    Quiz 4

--- Arancibia_Quiz_week4.R
+++ Arancibia_Quiz_week4.R
@@ -4,9 +4,7 @@
 
 library("ggplot2")
 library("plyr")
-library("grid")
-library("gridExtra")
-library("shiny")
+
 
 
 
@@ -14,7 +12,6 @@ m <- read.table("/users/bcarancibia/CUNY_IS_607/movies.tab", sep="\t", header=TR
 
 ####Question 1####
 ###Visualization that displays total number of movies for each decade
-m$decade <- as.factor((m$year %/% 10) * 10)
 q1 <- ggplot(data = m, aes(x=year)) + geom_bar(binwidth = 10) +ggtitle("Total Number of Movies for each Decade")
 q1
 
@@ -23,7 +20,7 @@ q1
 # Setting zeroes to NAs for easier calculating of the mean
 m[,c("Action", "Animation", "Comedy", "Drama", "Documentary", "Romance", "Short")][m[,c("Action", "Animation", "Comedy", "Drama", "Documentary", "Romance", "Short")] == 0] <- NA
 
-avg.ratings <- rbind(ddply(m[which(m$Action == 1),], c("year"), summarise, genre = "Action", rating = mean(rating)),
+ratings_average<- rbind(ddply(m[which(m$Action == 1),], c("year"), summarise, genre = "Action", rating = mean(rating)),
                      ddply(m[which(m$Animation == 1),], c("year"), summarise, genre = "Animation", rating = mean(rating)),
                      ddply(m[which(m$Comedy == 1),], c("year"), summarise, genre = "Comedy", rating = mean(rating)),
                      ddply(m[which(m$Drama == 1),], c("year"), summarise, genre = "Drama", rating = mean(rating)),
@@ -32,12 +29,11 @@ avg.ratings <- rbind(ddply(m[which(m$Action == 1),], c("year"), summarise, genre
                      ddply(m[which(m$Short == 1),], c("year"), summarise, genre = "Short", rating = mean(rating)))
 
 ggplot(avg.ratings, aes(x = year, y = rating, color = genre)) +
-  geom_line(size = 0.75) +
+  geom_line(size = 1) +
   facet_wrap(~genre) +
-  stat_smooth(method = lm, se = FALSE, color = "red", size = 0.75) +
-  xlab("Year of Release") +
-  ylab("Averge IMDB Rating") +
-  ggtitle("Avgerage IMDB Ratings by Movie Genre over Time") +
+  xlab("Release Year") +
+  ylab("Averge Rating") +
+  ggtitle("Avgerage Ratings by Genre over Time") +
   labs(color = "Genre") +
   theme(axis.title.x = element_text(size = 18),
         axis.text.x = element_text(size = 14),
@@ -56,45 +52,56 @@ length.rating
 #yikes lots of outliers
 cor(m$length, m$rating)
 
-m.short <- m[m$length < 500,]
-b<-ggplot(data=m.short, aes(x=length, y=jitter(rating)))
-b + geom_bin2d()
-c<-ggplot(data=m.short, aes(x=length, y=jitter(rating))) 
-c + geom_bin2d()
+m_no_outliers<- m[m$length < 500,]
+b<-ggplot(data=m_no_outliers, aes(x=length, y=jitter(rating))) + ggtitle("Length of Movie vs Rating")+
+  xlab("Length")+
+  ylab("Rating")
+b + geom_bin2d() 
+
 
 #Question 4
 #length of movie vs genre
-ggplot(data=m) + 
-  geom_boxplot(aes(x="Action", y=length[m$Action == 1]), outlier.shape = NA) + 
-  geom_boxplot(aes(x="Animation", y=length[m$Animation == 1]), outlier.shape = NA) + 
-  geom_boxplot(aes(x="Comedy", y=length[m$Comedy == 1]), outlier.shape = NA) + 
-  geom_boxplot(aes(x="Drama", y=length[m$Drama == 1]), outlier.shape = NA) + 
-  geom_boxplot(aes(x="Documentary", y=length[m$Documentary == 1]), outlier.shape = NA) + 
-  geom_boxplot(aes(x="Romance", y=length[m$Romance == 1]), outlier.shape = NA) + 
-  geom_boxplot(aes(x="Short", y=length[m$Short == 1]), outlier.shape = NA) +
-  ylim(0, 175) +
+c<-ggplot(data=m) + 
+  geom_boxplot(aes(x="Action", y=length[m$Action == 1]), outlier.shape = NA, fill ="grey80", color = "#3366FF") + 
+  geom_boxplot(aes(x="Animation", y=length[m$Animation == 1]), outlier.shape = NA, fill ="grey80", color = "#3366FF") + 
+  geom_boxplot(aes(x="Comedy", y=length[m$Comedy == 1]), outlier.shape = NA, fill ="grey80", color = "#3366FF") + 
+  geom_boxplot(aes(x="Drama", y=length[m$Drama == 1]), outlier.shape = NA, fill ="grey80", color = "#3366FF") + 
+  geom_boxplot(aes(x="Documentary", y=length[m$Documentary == 1]), outlier.shape = NA, fill ="grey80", color = "#3366FF") + 
+  geom_boxplot(aes(x="Romance", y=length[m$Romance == 1]), outlier.shape = NA, fill ="grey80", color = "#3366FF") + 
+  geom_boxplot(aes(x="Short", y=length[m$Short == 1]), outlier.shape = NA, fill ="grey80", color = "#3366FF") +
+  ylim(0, 150) +
   xlab("Genre") +
-  ylab("Length (in minutes)") +
-  ggtitle("Average length of movie by genre")
+  ylab("Length in Minutes") +
+  ggtitle("Average length of Movie by Genre")
+
+c+coord_flip()
+
 
 #Question 5
-m.180 <- subset(m, length > 50 & length < 180)
-length <- ggplot(data = m.180, aes(x = length, y = votes)) + 
-  geom_point() +
-  geom_smooth(method = "lm")
+#what other varible best predicts total number of votes that a movie received?
+m_subset_length <- subset(m, length > 50 & length < 180)
+#based on previous question, take a look at length that is more than 50 minutes and longer than 180
+length <- ggplot(data = m_subset_length, aes(x = length, y = votes)) + 
+  geom_point(aes(colour = rating), alpha=1/10) 
+length
 
 year <- ggplot(data = m, aes(x = year, y = votes)) + 
-  geom_point() +
-  geom_smooth(method = "lm")
+  geom_point(alpha=1/10)
+year
 
 rating <- ggplot(data = m, aes(x = rating, y = votes)) + 
-  geom_point() +
-  geom_smooth(method = "lm")
+  geom_point(aes(colour = rating), alpha=1/10) 
 
-m.budget <- subset(m, budget != 0)
+rating
 
-budget <- ggplot(data = m.budget, aes(x = budget, y = votes)) + 
-  geom_point() +
-  geom_smooth(method = "lm")
 
-grid.arrange(length, year, rating, budget, ncol = 2)
+m_budget_cannot_be_NA<- subset(m, budget != 0)
+
+budget <- ggplot(data = m_budget_cannot_be_NA, aes(x = budget, y = votes)) + 
+  geom_point(aes(colour = rating), alpha=1/10) 
+
+budget
+
+
+
+
