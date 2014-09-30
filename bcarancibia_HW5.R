4bc21243e0104265505f0853b8f30a33a8595692
bcarancibia <bcarancibia@gmail.com>
2014-09-30 15:19:42 -0400

    hw 5

--- bcarancibia_HW5.R
+++ bcarancibia_HW5.R
@@ -7,14 +7,15 @@ output: html_document
 #Question 1
 #Three questions that I want answer based on the data presented in the assignment are:
 #1: What are the total populations for each city so that the proportion for each response can be calculated.
-#2: What proportion of the respondents in each city voted yes or no?
-#3: What proportion of the respondents in each age group voted yes or no?
+#2: What are the total responses for yes and no in each city
+#3 What are the response for yes and response for no in each city
 #In order for questions 2 and 3 to be answered, you need the answer for question one.
 
 #Question 2
 require(tidyr)
 require(dplyr)
-require(ggplot2)
+require(plyr)
+
 
 #Create a dataframe to save this data in its current messy state.
 
@@ -43,7 +44,16 @@ tidy_df
 total_pop <- sum(tidy_df[, "votecount"])
 total_pop
 
+#2: What are the total responses for yes and no in each city
+city_sum <- plyr::ddply(tidy_df, "city", plyr::summarize, total = sum(votecount), .inform=TRUE)
+city_sum
 
+#3 What are the response for yes and response for no in each city
+sum_response_city <- plyr::ddply(tidy_df, c("city", "response"), plyr::summarize, sum = sum(votecount), .inform=TRUE)
+sum_response_city
 
+#Question 5
 
-
+#Having gone through the entire process, I would have asked different questions. I would have asked what is the actual proportion of the responses, not just total. 
+#I would change the dataframe if I did it again and creating variables out of city and age group. The way I did it now is pretty ugly.
+#Changing the way I structured the tidydata would have made it easier to answer the question relating to proportion of yes or no votes.
