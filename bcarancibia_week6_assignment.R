85d1b0a0e46a6e675222cb465592b11abe9c9f7f
bcarancibia <bcarancibia@gmail.com>
2014-10-06 19:29:51 -0400

    homework 6

---
+++ bcarancibia_week6_assignment.R
@@ -0,0 +1,15 @@
+#Benjamin Arancibia
+#October 6th, 2014
+#Week 6 Assignment
+
+#Pull in a website's information using rvest
+
+library(rvest)
+
+#I decided to look at my favorite TV show and extract the cast in it
+luther <- html("http://www.imdb.com/title/tt1474684/")
+
+cast <- luther %>%
+  html_nodes("#titleCast .itemprop span") %>%
+  html_text()
+cast
