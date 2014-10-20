8b6de7194375908c01a6e4d41047993987ad16a3
bcarancibia <bcarancibia@gmail.com>
2014-10-20 14:17:17 -0400

    week 8 HW

---
+++ Barancibia_Week8_assignment.sql
@@ -0,0 +1,10 @@
+--Ben Arancibia--
+--Week 8 Assignment--
+--October 20th, 2014--
+
+--Design a small database that tracks blog posts, comments, and tags--
+
+--Question 1 (blog)--
+-- http://www.theverge.com/ is a website/blog that has comments, blogposts, and tags.
+
+--Question 2(database design)--
