c752af13e7a5c9a5d319787b4646f6252bf8727c
bcarancibia <bcarancibia@gmail.com>
2014-11-17 21:55:48 -0500

    Week 12 Quiz

--- Week12/Barancibia_week12_quiz.sql
+++ Week12/Barancibia_week12_quiz.sql
@@ -29,4 +29,31 @@ MERGE (course:Course { department: csvLine.CourseDept, number: csvLine.CourseNum
 CREATE (student)-[:Enrolled {section: csvLine.Section, i
 nstructor: csvLine.Instructor, grade: csvLine.Grade}]->(course)
 
+--Question 2
+
+I think that a graph database is a bad choice. A relational database would be a better solution. 
+In an SQL database you would divide the data into three tables: Student, Course, and Housing. 
+The Student table will be the parent table and the Course table and the Housing table will be the child tables. 
+The Student table will hold the foreign key to those child tables. This is an easier solution in my opinion
+then having a graph database with different relationships. 
+If there had been loops in the graph, or other information that is difficult to capture in traditional relational structures, 
+then a graph database would have been a better choice.
+
+--Question 3
+
+MATCH (rm:Student)-[h:Housed]->(dorm:Dormitory)
+MATCH (rk:Student {firstname: "Richard", lastname: "Kowalski"})-[rmh:Housed]->(dorm)
+WHERE h.room = rmh.room AND rm <> rk
+RETURN rm
+
+--Question 4
+
+MATCH (rk:Student {id:"19457"})-[e:Enrolled]->(course:Course {number: "120", title: "Finite Mathematics"})
+CREATE (rk)-[c:Completed {
+section: e.section, 
+instructor: e.instructor, 
+grade: "B"}]->(course)
+
+DELETE e
+RETURN rk,c,course
 
